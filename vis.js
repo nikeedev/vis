6e3b9cc04b465242fba1fb2ca7e34902a0daedf1
nikeedev <nikita.gon.dev@gmail.com>
2024-12-04 12:50:34 +0100

    up

---
+++ vis.js
@@ -0,0 +1,75 @@
+// ==UserScript==
+// @name         VIS
+// @namespace    nikee.dev
+// @version      2.0
+// @description  make visma look good!
+// @author       nikeedev
+// @match        *://*.inschool.visma.no/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=visma.no
+// @grant        none
+// @license      GPL3.0
+// @run-at document-body
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.addEventListener('load', function() {
+        let css = document.createElement("style");
+        css.innerHTML = `
+        .top-menu-navbar {
+    background: #0659CE !important;
+}
+
+ul:first-of-type > :first-child {
+    display: none !important;
+}
+
+.navbar-brand {
+    border-right: 0 !important;
+}
+
+.navbar-menu-collapse {
+    flex: 0 !important;
+    padding-right: 35vh !important;
+}
+
+.navigation-search.search-input {
+    /*padding-left: 121px !important;*/
+}
+
+.search-input-content.vs-Input.form-control {
+    padding-left: 14vw !important;
+    min-width: 35vw !important;
+}
+
+.user-info > .text-size1 {
+    color: #a5c3ed !important;
+}
+
+#top-menu-navbar-brand {
+   font-family: monospace;
+}
+        `;
+
+        document.head.appendChild(css);
+
+        let search;
+
+        function run() {
+            search.placeholder = "[/] Søk på siden";
+        }
+
+        const waitForElement = setInterval(() => {
+            search = document.body.querySelector(".search-input-content.vs-Input.form-control");
+            if (search) {
+                clearInterval(waitForElement); // Stop checking
+                run();
+            }
+        }, 100);
+
+
+    });
+
+
+})();
